63685af3992c77ca5c0dc649ac56fa843af1e663
Shefali Lachhar <shefali.lachhar@gmail.com>
2019-10-01 20:51:01 +0530

    vidly with authentication

---
+++ VidlyWithAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyWithAuthentication.Startup))]
+namespace VidlyWithAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
